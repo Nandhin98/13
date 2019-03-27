32560efaeedae1e28b235df9b61eb9eb7dc8925e
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-27 16:22:13 +0530

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,8 @@
+from numpy import *
+               x = range(int(input("x")))
+                     x = reshape(x,(4,4))
+             print(x) 
+                n = int(input("enter row limits"))
+             m = int(input("enter col limits"))
+                               print("  ")
+                           print(a)
